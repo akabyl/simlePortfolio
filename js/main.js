3db34690ac94078a818d3fb52ef88280bd193775
Абду-Муталиб <abkabylbekov97@gmail.com>
2023-07-27 00:44:20 +0500

    end layout Portfolio

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const btnDarkMode = document.querySelector('.dark-mode-btn')
+
+
+if (localStorage.getItem('darkMode') === 'dark') {
+  btnDarkMode.classList.add('dark-mode-btn--active')
+  document.body.classList.add('dark')
+} 
+// else if (localStorage.getItem('darkMode') === 'light'){
+//   btnDarkMode.classList.add('dark-mode-btn--active')
+//   document.body.classList.add('dark')
+
+// }
+
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  btnDarkMode.classList.add('dark-mode-btn--active')
+  document.body.classList.add('dark')
+}
+
+
+
+
+
+
+btnDarkMode.onclick = function () {
+  btnDarkMode.classList.toggle("dark-mode-btn--active")
+  const isDark =  document.body.classList.toggle("dark")
+
+  if(isDark) {
+    localStorage.setItem('darkMode', 'dark')
+  } else {
+    localStorage.setItem('darkMode', 'light')
+  }
+
+
+}
+
